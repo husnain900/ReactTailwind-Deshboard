ca72d9be86b4f89435a74438b2c724df424ef3d9
husnain900 <husnain.tanveer123@gmail.com>
2024-02-01 10:15:13 +0000

    fixes

--- src/Components/Pages/RoutesPages.js
+++ src/Components/Pages/RoutesPages.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import Header from '../Commons/Header/Header';
-import MobileHeader from '../Commons/Header/MobileHeader';
-import Sidebar from '../Commons/SideBar/Sidebar';
-import Clients from './Clients/clients';
-import Home from './Home/home';
-import Product from './Product/product';
-import Orders from './Orders/orders';
-import Reports from './Reports/reports';
-import QA from './Q&A/Q&A';
-import PageStatic from './Static/pageStatic';
-import Coupons from './Coupons/coupon';
-import Commision from './Commision/commision';
-import Abandond from './AbandondCart/abandond';
-import Loyalty from './loyalty/loyalty';
-import Mahally from './Mahally/mahally';
-import StorePackege from './StorePackege/store';
-import SettingsPage from './SettingScreen/screen';
-import Wallet from './Wallet/wallet';
-import Storedesign from './StoreDesign/storedesign';
-import Maker from './AppMaker/maker';
-import Appmaker from './AppMaker2/appmaker';
-import Affiliate from './AffiliateMarketing/affilliate';
-import Affiliate2 from './AffiliateMarketing2/affiliate2';
-import Desktop23 from './Desktop23/desktop23';
-import HomePopup from '../PopUp/homepopup';
-import Footer from '../Commons/Footer/footer';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import Header from "../Commons/Header/Header";
+import MobileHeader from "../Commons/Header/MobileHeader";
+import Sidebar from "../Commons/SideBar/Sidebar";
+import Clients from "./Clients/clients";
+import Home from "./Home/home";
+import Product from "./Product/product";
+import Orders from "./Orders/orders";
+import Reports from "./Reports/reports";
+import QA from "./Q&A/Q&A";
+import PageStatic from "./Static/pageStatic";
+import Coupons from "./Coupons/coupon";
+import Commision from "./Commision/commision";
+import Abandond from "./AbandondCart/abandond";
+import Loyalty from "./loyalty/loyalty";
+import Mahally from "./Mahally/mahally";
+import StorePackege from "./StorePackege/store";
+import SettingsPage from "./SettingScreen/screen";
+import Wallet from "./Wallet/wallet";
+import Storedesign from "./StoreDesign/storedesign";
+import Maker from "./AppMaker/maker";
+import Appmaker from "./AppMaker2/appmaker";
+import Affiliate from "./AffiliateMarketing/affilliate";
+import Affiliate2 from "./AffiliateMarketing2/affiliate2";
+import Desktop23 from "./Desktop23/desktop23";
+import HomePopup from "../PopUp/homepopup";
+import Footer from "../Commons/Footer/footer";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function Routespages() {
   return (
     <Router>
       <div className="flex">
-        <div className="w-[20%] relative p-0 hidden lg:block">
+        <div className="lg:w-[24%] xl:w-[20%] relative p-0 hidden lg:block">
           <Sidebar />
         </div>
-        <div className="w-full lg:w-[80%]">
+        <div className="w-full lg:w-[76%] xl:w-[80%]">
           <div className="hidden lg:block">
             <Header />
           </div>
